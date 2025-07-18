483f58a9aa4e3196972dfa35313247c77288248f
vanshgajjar <vanshgajjar2007@gmail.com>
2025-07-18 22:59:13 +0530

    connection code

--- src/CarVerse.java
+++ src/CarVerse.java
@@ -1,6 +1,10 @@
-public class CarVerse {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class CarVerse {
+    public static void main(String[] args) throws Exception {
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/carrental","root","");
+        System.out.println((con!=null)?"success":"fail");
     }
 }
 class Car{
