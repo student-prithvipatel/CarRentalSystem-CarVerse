5e0cf1b751dda05e0dabdcee0f049ccd468b972a
student-prithvipatel <prithvitpatel@gmail.com>
2025-07-18 17:50:11 +0530

    Initial Files Added

---
+++ src/CarVerse.java
@@ -0,0 +1,20 @@
+public class CarVerse {
+    public static void main(String[] args) {
+
+    }
+}
+class Car{
+
+}
+class User{
+
+}
+class Buyer extends User{
+
+}
+class Admin extends User{
+
+}
+class Rental{
+
+}
